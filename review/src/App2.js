1273975c3780af6f884e3d055cd156775c57a88c
duellal <alexandriaduell@gmail.com>
2022-08-22 14:01:04 -0400

    Getting a class to render inside another class

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
+// const Person = (props) => {
+//    return (
+//       <div>
+//          <h1>
+//             Hello, {props.name} is {props.age}.
+//          </h1>
+//          <h3>
+//             {props.name} is a good name for a {props.age} year old.
+//          </h3>
+//       </div>
+//    )
+// }
+
+class Person extends React.Component {
+   // props is getting passed into the class of Person - to access the props, you need to do "this.props.(name of prop)"
+   render() {
+      return (
+         <div>
+            <h1>
+               Hello, {this.props.name} is {this.props.age}.
+            </h1>
+            <h3>
+               {this.props.name} is a good name for a {this.props.age} year old.
+            </h3>
+         </div>
+      )
+   }
+}
+
 class App2 extends React.Component {
    //Anything here is for the class - will not let you return things from here
 
@@ -25,9 +54,7 @@ class App2 extends React.Component {
 
       return (
          <div>
-            <h1>
-               Hello, {this.state.name} is {this.state.age}.
-            </h1>
+            <Person name={this.state.name} age={this.state.age} />
 
             <button onClick={this.clickHandler}>
                Click me to change state
